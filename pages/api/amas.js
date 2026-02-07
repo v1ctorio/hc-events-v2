024218938dde578118776e9fb2ccfbade8dbb489
Vic <vicxd@pm.me>
2026-02-07 23:33:44 +0100

    Init nuxt project

--- pages/api/amas.js
+++
@@ -1,8 +0,0 @@
-import { getEvents } from '../../lib/data'
-import { filter } from 'lodash'
-
-export default async (req, res) => {
-  let events = await getEvents()
-  events = filter(events, { ama: true })
-  res.json(events)
-}
--- pages/api/events/all-monthly.js
+++
@@ -1,8 +0,0 @@
-import { getEvents } from '../../../lib/data'
-import { groupBy } from 'lodash'
-
-export default async (req, res) => {
-  const events = await getEvents()
-  const months = groupBy(events, e => e.start.substring(0, 7))
-  res.json(months)
-}
