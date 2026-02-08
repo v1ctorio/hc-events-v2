3cb97444d1c5ea177f1a6176cf7b9e78fefb9c2b
Vic <vicxd@pm.me>
2026-02-08 10:12:03 +0100

    istg ts library doesn't want to work

---
+++ app/pages/authenticated.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const {loggedIn, user, login, logout} = useOidcAuth()
+</script>
+
+
+
+<template>
+  <div v-if="loggedIn">
+    <h1>Welcome {{ user?.userName }}!</h1>
+    <p>Logged in since {{ user?.loggedInAt }}</p>
+    <button @click="logout()">
+      Logout
+    </button>
+  </div>
+  <div v-else>
+    <h1>Not logged in</h1>
+    <a href="/auth/oidc/login">Login with GitHub</a>
+    <button @click="login()">
+      Login with default provider
+    </button>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', '@nuxthub/core'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxthub/core',
+    'nuxt-oidc-auth'
+  ],
 
   devtools: {
     enabled: true
@@ -13,6 +17,29 @@ export default defineNuxtConfig({
   },
 
   compatibilityDate: '2025-01-15',
+
+  hub: {
+    db: 'postgresql',
+  },
+
+  oidc: {
+    providers: {
+      oidc: {
+        clientId: process.env.NEXT_OIDC_PROVIDERS_OIDC_CLIENT_ID,
+        clientSecret: process.env.NEXT_OIDC_PROVIDERS_OIDC_CLIENT_SECRET,
+        /*authorizationUrl: "https://auth.hackclub.com/oauth/authorize",
+        userInfoUrl: "https://auth.hackclub.com/oauth/userinfo",
+        tokenUrl: "https://auth.hackclub.com/oauth/token",
+        redirectUri:"http://localhost:3000/auth/oidc/callback",
+        jwksUri: "https://auth.hackclub.com/oauth/discovery/keys",
+        issuer: "https://auth.hackclub.com",*/
+        scope: ["openid", "email"],
+        tokenRequestType: "form-urlencoded",
+        openIdConfiguration: "https://auth.hackclub.com/.well-known/openid-configuration"
+      }
+    }
+  }
+
 
   /*eslint: {
     config: {
